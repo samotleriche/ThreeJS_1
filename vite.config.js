4125ea159dd8ccd1287e1a287906fbc234b17858
samotleriche <samotleriche@gmail.com>
2023-04-27 19:52:46 -0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,20 @@ import { resolve } from "path";
 
 export default {
   root: "",
+  base: "./",
   server: {
     host: true,
     open: !isCodeSandbox, // Open if it's not a CodeSandbox
   },
   build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    sourcemap: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, "./index.html"),
         nested: resolve(__dirname, "./nested/index2.html"),
       },
     },
-    outDir: "../dist",
-    emptyOutDir: true,
-    sourcemap: true,
   },
 };
